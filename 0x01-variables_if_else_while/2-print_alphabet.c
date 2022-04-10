38319cb27520eab19376d4672bd95c21f718d530
sadiq-iy <abubakarogirima@gmail.com>
2022-04-10 00:26:24 -0700

    MOre

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c<= 'Z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
