1637c5c60f51e7ff5c216cf0f21f7015a5b79fc8
sadiq-iy <abubakarogirima@gmail.com>
2022-04-26 16:55:45 -0700

    More

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,5 +17,5 @@ void print_diagsums(int *a, int size)
 		a += size;
 	}
 	printf("%d, ", sum1);
-	printf("d\n", sum2);
+	printf("%d\n", sum2);
 }
--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -1,9 +1,11 @@
+#include "main.h"
+#include <stdio.h>
 /**
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of the memory to print
  * @size: the size of the memory to print
  *
- * Return; Nothing.
+ * Return: Nothing.
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
