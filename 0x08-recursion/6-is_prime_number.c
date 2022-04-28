e1915245fab15ce5a1392f834d8368afe0fdc4d9
sadiq-iy <abubakarogirima@gmail.com>
2022-04-28 14:40:57 -0700

    More

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,3 +19,14 @@ int prime_a(int a, int b)
 	}
 	return (prime_a(a, b + 1));
 }
+
+/**
+ * is_prime_number - main - chech the code
+ * @n: number
+ *
+ * Return: Always 0
+ */
+int is_prime_number(int n)
+{
+	return (prime_a(n, 2));
+}
